a55bdaef17db2440d98e15b026797a1687b98d1a
Veronika <gav51@ukr.net>
2024-05-17 15:16:37 +0200

    HW5

--- HW5.java
+++ HW5.java
@@ -1,2 +1,34 @@
-package PACKAGE_NAME;public class HW5 {
-}
+public class HW5 {
+
+
+    public static void main(String[] args) {
+        int first = 5;
+        int sec = 5;
+        int thr = 7;
+        int fourth = 8;
+
+        int a = first + sec;
+        int b = thr + fourth;
+
+        if (a > b) {
+            System.out.println("a is greater than b");
+        } else {
+            System.out.println("b greater than a");
+        }
+
+        int c = a + 1;
+        int d = b - 2;
+        System.out.println(c);
+        System.out.println(d);
+
+        String ex = (c > d) ? "true" : "false";
+        System.out.println("c is greater than d:"+" "+ex);
+
+        String odd  =((c % 2 != 0) || (d % 2 != 0)) ? "true" : "false";
+        System.out.println("c or d is odd:"+" "+odd);
+
+
+
+    }
+    }
+
